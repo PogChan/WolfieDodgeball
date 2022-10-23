4c851b84264b68d48ef6202adf2cc60989044792
AllinaChan <43279583+AllinaChan@users.noreply.github.com>
2022-10-22 21:06:35 -0400

    Changed folder structure

--- EnemyFBX/Source/WolfieDodgeball/WolfieDodgeball.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "WolfieDodgeball.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WolfieDodgeball, "WolfieDodgeball" );
